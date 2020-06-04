0e77190ff3d9948548d0ab37def339c3cf9ab02b
Parham <parham.fazlali.dev@gmail.com>
2020-06-04 10:28:04 +0430

    add helper for rendering html

---
+++ src/helpers/html.ts
@@ -0,0 +1,17 @@
+export default () => {
+  const html = `
+    <!doctype html>
+    <html>
+      <head>
+
+      </head>
+
+      <body>
+        Hello World!
+
+      </body>
+    </html>
+  `;
+
+  return html;
+}
--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,9 @@
 import express from 'express';
+import html from './helpers/html';
 
 const app = express();
 
-app.get("*", (req, res) => res.send('Hello World!'));
+app.get("*", (req, res) => res.send(html()));
 
 app.listen(3000, 'localhost', (err) => {
   if (err) console.error(err);
