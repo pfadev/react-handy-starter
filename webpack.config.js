ec864d058632a0f5dd0484eb56909bf02dff3fa8
Parham <parham.fazlali.dev@gmail.com>
2020-06-04 22:20:33 +0430

    set NODE_ENV in scripts

--- webpack.config.js
+++ webpack.config.js
@@ -1,33 +1,32 @@
 const path = require('path');
 
-module.exports = (env, argv) => {
-  const isDev = argv.mode === 'development';
+const mode = process.env.NODE_ENV || 'development';
+const isDev = mode === 'development';
 
-  return {
-    devtool: isDev ? 'eval-source-map' : false,
-    entry: './src/client.tsx',
-    module: {
-      rules: [
-        {
-          test: /\.(j|t)s(x)$/,
-          exclude: /node_modules/,
-          use: {
-            loader: require.resolve('babel-loader'),
-            options: {
-              presets: ['@babel/preset-env'],
-              cacheDirectory: isDev,
-            }
+module.exports = {
+  devtool: isDev ? 'eval-source-map' : false,
+  entry: './src/client.tsx',
+  mode,
+  module: {
+    rules: [
+      {
+        test: /\.(j|t)s(x)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: require.resolve('babel-loader'),
+          options: {
+            presets: ['@babel/preset-env'],
+            cacheDirectory: isDev,
           }
-        },
-      ]
-    },
-    output: {
-      filename: isDev ? "[name].js" : "[name].[hash:8].js",
-      path: path.resolve(process.cwd(), 'dist'),
-    },
-    resolve: {
-      extensions: ['.js', '.jsx', '.json']
-    },
-  };
-}
-
+        }
+      },
+    ]
+  },
+  output: {
+    filename: isDev ? "[name].js" : "[name].[hash:8].js",
+    path: path.resolve(process.cwd(), 'dist'),
+  },
+  resolve: {
+    extensions: ['.js', '.jsx', '.json', '.ts', '.tsx']
+  },
+};
