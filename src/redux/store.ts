63501c0589e315adc481d156c7b7f78655483424
Parham <parham.fazlali.dev@gmail.com>
2020-06-24 06:54:36 +0430

    enable __REDUX_DEVTOOLS_EXTENSION_COMPOSE__ in development mode

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -5,7 +5,14 @@ import reducer from "./reducers";
 export default () => {
   const middlewares = [];
 
-  const enhancers = compose(applyMiddleware(...middlewares));
+  const devCompose =
+    DEV &&
+    typeof window === "object" && // @ts-ignore
+    window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__;
+
+  const composeEnhancers = devCompose || compose;
+
+  const enhancers = composeEnhancers(applyMiddleware(...middlewares));
 
   const store = createStore(reducer, {}, enhancers);
 
