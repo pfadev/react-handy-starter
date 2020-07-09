9301b1df4163613a3c80e74c6ebf3c31122287ba
Parham <parham.fazlali.dev@gmail.com>
2020-07-10 00:32:02 +0430

    add params & query to renderRoutes

--- src/app/app.tsx
+++ src/app/app.tsx
@@ -1,18 +1,28 @@
 import React from "react";
-import { renderRoutes } from "react-router-config";
+import { matchRoutes, renderRoutes } from "react-router-config";
 import { Helmet } from "react-helmet";
 import { Offline, Online } from "react-detect-offline";
 
 import { NoInternet } from "../components";
 import config from "../config";
+import urlHelper from "../helpers/url.helper";
 import StyleWrapper from "./app.style";
 
-export default ({ route }: any) => (
-  <StyleWrapper>
-    <Helmet {...config.app} />
-    <Offline>
-      <NoInternet />
-    </Offline>
-    <Online>{renderRoutes(route.routes)}</Online>
-  </StyleWrapper>
-);
+export default ({ location, route }: any) => {
+  const params = matchRoutes(route.routes, location.pathname).reduce(
+    (acc, { match: { params: cur } }) => ({ ...acc, ...cur }),
+    {}
+  );
+
+  const query = urlHelper.parse(location);
+
+  return (
+    <StyleWrapper>
+      <Helmet {...config.app} />
+      <Offline>
+        <NoInternet />
+      </Offline>
+      <Online>{renderRoutes(route.routes, { params, query })}</Online>
+    </StyleWrapper>
+  );
+};
